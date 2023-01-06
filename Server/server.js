32379489e4aa567ec3911dfbbe527d6b9fecf5a4
roboryakhel <roboryakhel@outlook.com>
2023-01-05 20:04:02 -0500

    updated server and client

--- Server/server.js
+++ Server/server.js
@@ -11,11 +11,12 @@ const io = require('socket.io')(server,{
     credentials: true
   }
 });
-//cconst io = socketio(server);
+
 const usernames = ["bob", "alice", "sam", "john"];      // Server has list of usernames that it uses to randomly assign to clients
 const mainRoom = "Main";            // All clients initally connect to main room
 
 let rooms = [];                     // List of rooms that the server has
+const users = new Map();
 
 // client connects
 io.on("connection", socket => {
@@ -34,61 +35,73 @@ io.on("connection", socket => {
     });
     gotoroom(socket, roomID, username);
     console.log("Socket: ",socket.id, " connected to this server",'\n',"has name: ",username, " and roomID: ",roomID,'\n'); 
-  } else {
+  } 
+  else {
     let r = socket.handshake.headers.roomid;
-    socket.join(r);
-    let username = genUsername();
-    socket.emit(r, {
-      username: username,
-      type: "sub",
-      roomID: r
-    }); 
-    console.log("Socket: ",socket.id, " connected to this server",'\n',"has name: ",username, " and roomID: ",r,'\n'); 
+    if (!rooms.includes(r)) {
+      console.log("Bad connection attempt. Disconnecting...");
+      socket.emit(r,"Room does not exist");
+    } else {
+      socket.join(r);
+      let username = genUsername();
+      socket.emit(r, {
+        username: username,
+        type: "sub",
+        roomID: r
+      }); 
+      users.set(socket.id, r);
+      console.log("Socket: ",socket.id, " connected to this server",'\n',"has name: ",username, " and roomID: ",r,'\n'); 
+    }
   }
+
+  socket.on('Video Time', (message) => {
+    const uID = socket.id;
+  
+    io.to(users.get(uID)).emit('VTL', message);
+    console.log (message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.id +" disconnected");
+    users.delete(socket.id);
+    // const user = removeUser(socketio.id);
+  
+    // if (user) {
+    //   io.to(user.room).emit('message', {
+    //     user: 'adminX',
+    //     text: `${user.name.toUpperCase()} has left.`
+    //   });
+    //   io.to(user.room).emit('roomData', {
+    //     room: user.room,
+    //     users: getUsersInRoom(user.room)
+    //   });
+    // }
+  });
+
+  console.log(users);
 });
 
 
-function gotoroom(socket, roomID, username) {
+
+const gotoroom = (socket, roomID, username) => {
   socket.join(roomID);
   socket.emit(roomID,{
     text: "Success"
   });
+  users.set(socket.id, roomID);
 }
 
-// socketio.on('sendMessage', (message, callback) => {
-//   const user = getUser(socketio.id);
-
-//   io.to(user.room).emit('message', { user: user.name, text: message });
-
-//   callback();
-// });
-
-// socketio.on('disconnect', () => {
-//   const user = removeUser(socketio.id);
-
-//   if (user) {
-//     io.to(user.room).emit('message', {
-//       user: 'adminX',
-//       text: `${user.name.toUpperCase()} has left.`
-//     });
-//     io.to(user.room).emit('roomData', {
-//       room: user.room,
-//       users: getUsersInRoom(user.room)
-//     });
-//   }
-// });
+// TODO: make this generate unique name
+const genUsername = () => {
+  return usernames[Math.floor(Math.random() * usernames.length)];
+}
+// TODO: make this generate unique id
+const genRoomID = () => {
+  let roomID = Date.now().toString(36) + Math.random().toString(36).substr(2);
+  rooms.push(roomID);
+  return roomID;
+}
 
 server.listen(process.env.PORT || 8080, () =>
   console.log('Server is running')
 );
-
-
-const genUsername = () => {
-  return usernames[Math.floor(Math.random() * usernames.length)];
-}
-
-const genRoomID = () => {
-  let roomID = Math.random().toString(10).slice(2);
-  rooms.push(roomID);
-  return roomID;
-}
--- vid-sync-client/src/Components/Home/index.js
+++ vid-sync-client/src/Components/Home/index.js
@@ -3,85 +3,110 @@ import {Container, Wrapper, RoomIDInp, ConnectBtn, Name, Type, RoomMembers, Pick
 import { io } from "socket.io-client";
 
 const URL = "ws://localhost:8080";
+let socket = "";
 
 let type ="";                           // pub or sub
 let username = "";                      // Server will give client randomly generated user name
 let roomID = "";                        // Servier will give a n digit identifier for your room
 
-// class Client extends React.Component {
+export const Home = () => {
+    const [uname, setUname] = useState("");
+    const [room, setRoomID] = useState("0");
 
-//     constructor() {
-//         super();
-//         this.state = {type: "pub", username: "", roomID: ""};
-//     }
-
-
-
-    // Friend sends pubRoomID via msg and this client get put in that room.
-  const  connectSubscriber = (pubRoomID) => {
-            const socket = io(URL, {
-                    extraHeaders: {
-                    type: "sub",
-                    roomID: pubRoomID
-                    }
-                });
-            socket.on(pubRoomID, (args) => {
-                    console.log(args);
-                    username = args.username
-                    roomID = args.roomID;
-                    type = args.type;
-                    console.log(username," connected to room ", roomID, " as ", type);
-            });
+    const handleInput = (event) => {
+        roomID = event.target.value;
+        //console.log(roomIDisValid());
     }
 
+    const roomIDisValid = () => {
+        return (roomID !== "") && (roomID.length === 16) && (!isNaN(roomID));
+    }
 
+    const connect = () => {
+        if (roomIDisValid()) 
+            connectSubscriber(roomID);
+        else  {
+            console.log("Room ID invlid or No RoomID. Connecting is room host...");
+            connectPublisher();
+        } 
+    }
+    // Friend sends pubRoomID via msg and this client get put in that room.
+    const connectSubscriber = (pubRoomID) => {
+        socket = io(URL, {
+            extraHeaders: {
+            type: "sub",
+            roomID: pubRoomID
+            }
+        });
+       socket.on(pubRoomID, (args) => {
+             if (args === "Room does not exist") {
+                console.log("Could not connect to server. Room does not exist.");
+                socket.on("disconnect", () => {
+                    console.log("Disconnected");
+                });              
+            }
+             else {
+                console.log(args);
+                username = args.username
+                roomID = args.roomID;
+                type = args.type;
+                console.log(username," connected to room ", roomID, " as ", type);
+            }
+        });
+    }
+    
+    // Connect to Main room and get username, and roomID, then go to my own room
+    const connectPublisher = () => {
+        socket = io(URL, {
+            withCredentials: true,
+            extraHeaders: {
+                "type": "pub"
+            }
+        });
+        console.log(socket.id, " connecting to Main room in server")
+        socket.on("Main", (args) => {
+            username = args.username
+            roomID = args.roomID;
+            type = args.type;
+            console.log(username," connected to Main room ", roomID, " as ", type);
+            putPubinTheirRoom(socket);
+        });        
+        setUname(username);
+        setRoomID(roomID);
+    }
 
     // Connect to client room and send init msg
- function   putPubinTheirRoom(socket) {
-            socket.on(roomID, (args) => {
-                    if (args.text === "Success")
-                            console.log("You are now in room: ", roomID, ", ", username);
-            });
+    const putPubinTheirRoom = (socket) => {
+        socket.on(roomID, (args) => {
+            if (args.text === "Success")
+                console.log("You are now in room: ", roomID, ", ", username);
+        });
     }
 
-    export const Home = () => {
-        const [uname, setUname] = useState("");
-        const [room, setRoomID] = useState("0");
+    const sendTestMsg = () => {
+        socket.emit('Video Time', {
+            time: "34:12:00"
+          });
 
-        // Connect to Main room and get username, and roomID, then go to my own room
-        const connectPublisher = () => {
-            const socket = io(URL, {
-                withCredentials: true,
-                extraHeaders: {
-                "type": "pub"
-                }
-            });
-            console.log(socket.id, " connecting to Main room in server")
-            socket.on("Main", (args) => {
-                    username = args.username
-                    roomID = args.roomID;
-                    type = args.type;
-                    console.log(username," connected to Main room ", roomID, " as ", type);
-                    putPubinTheirRoom(socket);
-            });
-            setUname(username);
-            setRoomID(roomID);
-        }
-
-        return (
-            <>
-             <Wrapper>
-                 <Container>
-                     <RoomIDInp></RoomIDInp>
-                     <ConnectBtn onClick={connectPublisher}>Connect</ConnectBtn>
-                     <Name>Name: {uname} </Name>
-                     <Type>RoomID: {room} </Type>
-                     <RoomMembers></RoomMembers>
-                     <PickMovie></PickMovie>
-                 </Container>
-             </Wrapper>
-            </>
-           );
+          socket.on('VTL', (args) => {
+            console.log(args);
+          });
     }
-//}
 
+
+    return (
+        <>
+            <Wrapper>
+                <Container>
+                    <RoomIDInp type="text" onChange={handleInput}></RoomIDInp>
+                    <ConnectBtn onClick={connect}>Connect</ConnectBtn>
+                    <Name>Name: {uname} </Name>
+                    <Type>RoomID: {room} </Type>
+                    <ConnectBtn onClick={sendTestMsg}>Send</ConnectBtn>
+                    <RoomMembers></RoomMembers>
+                    <PickMovie></PickMovie>
+                </Container>
+            </Wrapper>
+        </>
+    );
+}
