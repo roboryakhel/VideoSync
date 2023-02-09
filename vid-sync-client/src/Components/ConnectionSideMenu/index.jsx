2d0e112401939881a93692fca364f451fe36f06f
roboryakhel <roboryakhel@outlook.com>
2023-02-09 18:37:42 -0500

    minor change

--- vid-sync-client/src/Components/ConnectionSideMenu/index.jsx
+++ vid-sync-client/src/Components/ConnectionSideMenu/index.jsx
@@ -2,8 +2,7 @@ import React, {useState, useRef} from 'react';
 import {Container, Wrapper, Btn, ContainerInner, Horizontal,Icon, ChatContainer, SelVidIcon, MsgBox, MsgInputWrapper, MsgInput, MsgSendButton } from './elements';
 import { FaBars, FaUsers, FaLink, FaUserAlt, FaFileVideo } from "react-icons/fa";
 import {AiOutlineDisconnect} from "react-icons/ai";
-import { Socket } from 'socket.io-client';
-
+import alien from "../../Pictures/alien.png"
 export const ConnectionSideMenu = (props) => {
     const [sidebarOpen, setSideBarOpen] = useState(false);
     const inputFile = useRef(null);
@@ -36,10 +35,21 @@ export const ConnectionSideMenu = (props) => {
                     <ChatContainer >
                         <MsgBox className={sidebarInnerClass}>
                             {info}
-                            {props.chat}
+                            {/* make the below into a separate component */}
+                            {/* <article className={"msg-container msg-self"} id={"msg-0"}>
+                                <div className={"msg-box"}>
+                                    <div className={"flr"}>
+                                        <div className={"messages"}>
+                                            <p className={"msg"} id={"msg-1"}> {props.chat} </p>
+                                        </div>
+                                        <span className={"timestamp"}><span className={"username"}>Name</span>&bull;<span className={"posttime"}>Now</span></span>
+                                    </div>
+                                    <img className={"user-img"} id={"user-0"} src={alien} />
+                                </div>
+			                </article> */}
                         </MsgBox>
                         <MsgInputWrapper className={sidebarInnerClass}>
-                            <MsgInput       className={"msgInput"}   
+                            <MsgInput  className={"msgInput"}   
                                 type="text"
                                 id="message"
                                 name="message"
@@ -54,7 +64,7 @@ export const ConnectionSideMenu = (props) => {
                     </ChatContainer>
                     <div className={sidebarInnerClass}>
                         <div id="disconnectBtnWrapper">
-                            <Btn className={"disconnectBtn"} onClick={()=>{props.disc()}}><AiOutlineDisconnect style={{"font-size":"25px"}}></AiOutlineDisconnect></Btn>
+                            <Btn className={"disconnectBtn"} onClick={()=>{props.disc()}}><AiOutlineDisconnect style={{"fontSize":"25px"}}></AiOutlineDisconnect></Btn>
                         </div>
                     </div>
                 </Container>
@@ -62,31 +72,3 @@ export const ConnectionSideMenu = (props) => {
         </>
     );
 }
-
-
-
-// <body>
-// 	<section class="chatbox">
-// 		<section class="chat-window">
-// 			<article class="msg-container msg-self" id="msg-0">
-// 				<div class="msg-box">
-// 					<div class="flr">
-// 						<div class="messages">
-// 							<p class="msg" id="msg-1">
-// 								Lorem ipsum
-// 							</p>
-// 						</div>
-// 						<span class="timestamp"><span class="username">Name</span>&bull;<span class="posttime">Now</span></span>
-// 					</div>
-// 					<img class="user-img" id="user-0" src="//gravatar.com/avatar/56234674574535734573000000000001?d=retro" />
-// 				</div>
-// 			</article>
-// 		</section>
-// 		<form class="chat-input" onsubmit="return false;">
-// 			<input type="text" autocomplete="on" placeholder="Type a message" />
-// 			<button>
-//                     <svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="rgba(0,0,0,.38)" d="M17,12L12,17V14H8V10H12V7L17,12M21,16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V7.5C3,7.12 3.21,6.79 3.53,6.62L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.79,6.79 21,7.12 21,7.5V16.5M12,4.15L5,8.09V15.91L12,19.85L19,15.91V8.09L12,4.15Z" /></svg>
-//                 </button>
-// 		</form>
-// 	</section>
-// </body>
