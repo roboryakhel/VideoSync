f8428d9942589cfe7f8498cc2ab31688f796c686
roboryakhel <roboryakhel@outlook.com>
2023-02-09 17:53:58 -0500

    UI update

--- vid-sync-client/src/Components/ConnectionSideMenu/elements.js
+++ vid-sync-client/src/Components/ConnectionSideMenu/elements.js
@@ -31,15 +31,10 @@ export const Horizontal = styled.div`
 `;
 
 export const Icon = styled.div`
-    // font-size: 24px;
-    // cursor: pointer;
-`;
+    font-size: 18px;
+    cursor: pointer;
+    color:#B6EADA;
 
-export const ChatContainer = styled.div`
-    width:100%;
-    height:68%;
-    margin:auto;
-    background-color: #c5c7c9;
 `;
 
 export const SelVidIcon = styled.div`
@@ -47,27 +42,68 @@ export const SelVidIcon = styled.div`
     cursor: pointer;
 `;
 
-export const MsgBox = styled.div`
-    height: 90%;
-    overflow:scroll;
-    border:1px solid black;
-    background-color: white;
+export const ChatContainer = styled.div`
+    width:100%;
+    height:50%;
     margin:auto;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    box-shadow: 0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28);
+`;
+
+
+export const MsgBox = styled.div`
+    // height: 90%;
+    width:100%;
+    // overflow:scroll;
+    background-color: #E7F6F2;
+    // margin:auto;
+    flex: auto;
+    max-height: calc(100% - 60px);
+    background: #2f323b;
+    overflow: auto;
+}
 `;
 
 export const MsgInputWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-item: center; 
-    border:1px solid blue;
-    height:10%;
+    // display: flex;
+    // flex-direction: row;
+    // justify-content: space-between;
+    // align-item: center; 
+    // margin:auto;
+    // width:85%;
+    // height:5%;
+    flex: 0 0 auto;
+    height: 60px;
+    background: #40434e;
+    border-top: 1px solid #2671ff;
+    box-shadow: 0 0 4px rgba(0,0,0,.14),0 4px 8px rgba(0,0,0,.28);
 `;
 
 export const MsgInput = styled.input`
-    width : 80%;
+    // width : 80%;
+    // border-bottom-left-radius: 8px;
+    line-height: 60px;
+    outline: 0 none;
+    border: none;
+    width: calc(100% - 60px);
+    color: white;
+    text-indent: 10px;
+    font-size: 12pt;
+    padding: 0;
+    background: #40434e;
 `;
 
 export const MsgSendButton = styled.button`
-    width 20%;
+    float: right;
+    outline: 0 none;
+    border: none;
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
+    padding: 2px 0 0 0;
+    margin: 10px;
+    transition: all 0.15s ease-in-out;
+
 `;
--- vid-sync-client/src/Components/ConnectionSideMenu/index.jsx
+++ vid-sync-client/src/Components/ConnectionSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useRef} from 'react';
 import {Container, Wrapper, Btn, ContainerInner, Horizontal,Icon, ChatContainer, SelVidIcon, MsgBox, MsgInputWrapper, MsgInput, MsgSendButton } from './elements';
 import { FaBars, FaUsers, FaLink, FaUserAlt, FaFileVideo } from "react-icons/fa";
+import {AiOutlineDisconnect} from "react-icons/ai";
 import { Socket } from 'socket.io-client';
 
 export const ConnectionSideMenu = (props) => {
@@ -20,37 +21,72 @@ export const ConnectionSideMenu = (props) => {
             <Wrapper className={sidebarClass}>
                 <Container>
                     <Horizontal className={"openCloseBar"}>
-                        <Icon onClick={handleViewSidebar}><FaBars className={"FaBars"}></FaBars></Icon>
-                        <Icon className={sidebarInnerClass}><FaLink onClick={props.copyURL}></FaLink></Icon>
-                        <Icon className={sidebarInnerClass}><FaUsers onClick={a}></FaUsers></Icon>
-                        <Icon className={sidebarInnerClass}><FaUserAlt onClick={b}></FaUserAlt></Icon>
+                        <Icon onClick={handleViewSidebar}><FaBars style={{"color":"#FFFBEB"}} className={"FaBars"}></FaBars></Icon>
+                        <Icon className={sidebarInnerClass}><FaLink style={{"color":"#FFFBEB"}} onClick={props.copyURL}></FaLink></Icon>
+                        <Icon className={sidebarInnerClass}><FaUsers style={{"color":"#FFFBEB"}} onClick={a}></FaUsers></Icon>
+                        <Icon className={sidebarInnerClass}><FaUserAlt style={{"color":"#FFFBEB"}} onClick={b}></FaUserAlt></Icon>
                     </Horizontal>
                     <ContainerInner className={sidebarInnerClass} >
                         <Horizontal className={"startBar"}>
                             <Btn className={"button-strt button- startPty"} onClick={()=>{props.con()}}>Start a Party</Btn>
-                            <SelVidIcon><FaFileVideo onClick={()=>{selectMovie()}}></FaFileVideo></SelVidIcon>
+                            <SelVidIcon><FaFileVideo style={{"color":"#00FFF6"}} onClick={()=>{selectMovie()}}></FaFileVideo></SelVidIcon>
                             <input type='file' id='file' ref={inputFile} style={{display: 'none'}} onChange={props.chURL}/>
                         </Horizontal>
                     </ContainerInner>
-                    <ChatContainer className={sidebarInnerClass}>
-                        <MsgBox>
+                    <ChatContainer >
+                        <MsgBox className={sidebarInnerClass}>
                             {info}
                             {props.chat}
                         </MsgBox>
-                        <MsgInputWrapper>
-                            <MsgInput         
+                        <MsgInputWrapper className={sidebarInnerClass}>
+                            <MsgInput       className={"msgInput"}   
                                 type="text"
                                 id="message"
                                 name="message"
+                                placeholder="Type a message"
                                 onChange={props.handleMsgInput}
                                 >
                             </MsgInput>
-                            <MsgSendButton onClick={props.sendMsg}>Send</MsgSendButton>
+                            <MsgSendButton className={"sendMsgBtn"} onClick={props.sendMsg}>
+                                <svg style={{"width":"24px","height":"24px"}} viewBox="0 0 24 24"><path fill="rgba(0,0,0,.38)" d="M17,12L12,17V14H8V10H12V7L17,12M21,16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V7.5C3,7.12 3.21,6.79 3.53,6.62L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.79,6.79 21,7.12 21,7.5V16.5M12,4.15L5,8.09V15.91L12,19.85L19,15.91V8.09L12,4.15Z" /></svg>
+                            </MsgSendButton>
                         </MsgInputWrapper>
                     </ChatContainer>
-                    <Btn className={"button-disc button- disconnect"} onClick={()=>{props.disc()}}>Disconnect</Btn>
+                    <div className={sidebarInnerClass}>
+                        <div id="disconnectBtnWrapper">
+                            <Btn className={"disconnectBtn"} onClick={()=>{props.disc()}}><AiOutlineDisconnect style={{"font-size":"25px"}}></AiOutlineDisconnect></Btn>
+                        </div>
+                    </div>
                 </Container>
             </Wrapper>
         </>
     );
 }
+
+
+
+// <body>
+// 	<section class="chatbox">
+// 		<section class="chat-window">
+// 			<article class="msg-container msg-self" id="msg-0">
+// 				<div class="msg-box">
+// 					<div class="flr">
+// 						<div class="messages">
+// 							<p class="msg" id="msg-1">
+// 								Lorem ipsum
+// 							</p>
+// 						</div>
+// 						<span class="timestamp"><span class="username">Name</span>&bull;<span class="posttime">Now</span></span>
+// 					</div>
+// 					<img class="user-img" id="user-0" src="//gravatar.com/avatar/56234674574535734573000000000001?d=retro" />
+// 				</div>
+// 			</article>
+// 		</section>
+// 		<form class="chat-input" onsubmit="return false;">
+// 			<input type="text" autocomplete="on" placeholder="Type a message" />
+// 			<button>
+//                     <svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="rgba(0,0,0,.38)" d="M17,12L12,17V14H8V10H12V7L17,12M21,16.5C21,16.88 20.79,17.21 20.47,17.38L12.57,21.82C12.41,21.94 12.21,22 12,22C11.79,22 11.59,21.94 11.43,21.82L3.53,17.38C3.21,17.21 3,16.88 3,16.5V7.5C3,7.12 3.21,6.79 3.53,6.62L11.43,2.18C11.59,2.06 11.79,2 12,2C12.21,2 12.41,2.06 12.57,2.18L20.47,6.62C20.79,6.79 21,7.12 21,7.5V16.5M12,4.15L5,8.09V15.91L12,19.85L19,15.91V8.09L12,4.15Z" /></svg>
+//                 </button>
+// 		</form>
+// 	</section>
+// </body>
