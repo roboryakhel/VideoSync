8707cdc32a478fe0dcd9d917adf68a8fa5fbd523
Socket <roboryakhel@outlook.com>
2023-05-28 22:45:01 -0400

    Add files via upload

--- vid-sync-client/src/Components/Button/index.jsx
+++ vid-sync-client/src/Components/Button/index.jsx
@@ -1,1 +1,22 @@
+import React, { useEffect } from "react";
+import { AiOutlineDisconnect } from "react-icons/ai";
+import { Btn } from "../ConnectionSideMenu/elements";
 
+export const Button = (props) => {
+  const disconnect = () => {
+    if (props.connected) {
+      props.handleDisconnect();
+    }
+  };
+  return (
+    <>
+      <div id="disconnectBtnWrapper">
+        <Btn className={"disconnectBtn"} onClick={disconnect}>
+          <AiOutlineDisconnect
+            style={{ fontSize: "25px" }}
+          ></AiOutlineDisconnect>
+        </Btn>
+      </div>
+    </>
+  );
+};
